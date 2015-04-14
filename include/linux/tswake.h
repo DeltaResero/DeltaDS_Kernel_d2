eecabc71cef96882122b22510eedae3aa35b139b
Ryan Pennucci <decimalman@gmail.com>
2015-04-13 20:15:12 -0400

    include: fix tswake.h derp

--- include/linux/tswake.h
+++ include/linux/tswake.h
@@ -1,5 +1,7 @@
 #ifndef _TSWAKE_H
 #define _TSWAKE_H
+
+#ifdef CONFIG_TSWAKE
 /* input & i2c drivers need to register a few bits with tswake */
 void tswake_notify_pwrkey(struct input_dev *dev);
 void tswake_notify_resolution(int x, int y);
@@ -21,4 +23,5 @@ static inline int tswake_active(void) { return 0; }
 static inline int tswake_enabled(void) { return 0; }
 static inline void tswake_touch_event(int x, int y) { }
 void tswake_i2c_enable(void) { }
+#endif
 #endif
