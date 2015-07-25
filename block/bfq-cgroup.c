b21a2d0c8d90182c9d27b8e7b581717da7a91094
Ryan Pennucci <decimalman@gmail.com>
2015-07-25 14:09:26 -0400

    block: fix bfq cgroups

--- block/bfq-cgroup.c
+++ block/bfq-cgroup.c
@@ -729,13 +729,8 @@ static struct cftype bfqio_files[] = {
 		.read_u64 = bfqio_cgroup_ioprio_class_read,
 		.write_u64 = bfqio_cgroup_ioprio_class_write,
 	},
+	{ }
 };
-
-static int bfqio_populate(struct cgroup_subsys *subsys, struct cgroup *cgroup)
-{
-	return cgroup_add_files(cgroup, subsys, bfqio_files,
-				ARRAY_SIZE(bfqio_files));
-}
 
 static struct cgroup_subsys_state *bfqio_create(struct cgroup *cgroup)
 {
@@ -848,8 +843,8 @@ struct cgroup_subsys bfqio_subsys = {
 	.can_attach = bfqio_can_attach,
 	.attach = bfqio_attach,
 	.destroy = bfqio_destroy,
-	.populate = bfqio_populate,
 	.subsys_id = bfqio_subsys_id,
+	.base_cftypes = bfqio_files,
 };
 #else
 static inline void bfq_init_entity(struct bfq_entity *entity,
