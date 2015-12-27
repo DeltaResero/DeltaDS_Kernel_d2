1bd3d5032bc3884f56358fb96229482eda73dacf
Ryan Pennucci <decimalman@gmail.com>
2015-12-26 21:39:54 -0500

    cpufreq: tweak tierservative defaults
    Previous defaults had trouble with screen-off audio playback.

--- drivers/cpufreq/cpufreq_tierservative.c
+++ drivers/cpufreq/cpufreq_tierservative.c
@@ -64,8 +64,8 @@ struct ts_global_priv {
 };
 
 static struct ts_global_priv ts_global __read_mostly = {
-	.tier_count = 8,
-	.extra_khz = 25000,
+	.tier_count = 6,
+	.extra_khz = 33000,
 	.sample_time = MS(16),
 	.max_sample = 333,
 	.active_sample = 100,
