881765e3a3d8f95cd4be091398b9ca9b8aa425bf
Junjie Wu <junjiew@codeaurora.org>
2016-01-05 11:09:00 -0800

    cpufreq: skateractive: Use wake_up_process_no_notif to wake up tasks Scheduler could send a notification to governor each time a task wakes up. If governor wakes up another task as a response to such a notification, it could result in endless recursive notifications.
    Use wake_up_process_no_notif to ensure scheduler won't send another
    notification for speedchange task woken up by the governor.
    
    Change-Id: I697affcbdf79e2ad0cfe843eb880d304960682f4
    Signed-off-by: Junjie Wu <junjiew@codeaurora.org>
    Signed-off-by: Lonelyoneskatter <threesixoh.skater@yahoo.com>

--- drivers/cpufreq/cpufreq_skateractive.c
+++ drivers/cpufreq/cpufreq_skateractive.c
@@ -571,7 +571,7 @@ static void cpufreq_skateractive_timer(unsigned long data)
 	spin_lock_irqsave(&speedchange_cpumask_lock, flags);
 	cpumask_set_cpu(data, &speedchange_cpumask);
 	spin_unlock_irqrestore(&speedchange_cpumask_lock, flags);
-	wake_up_process(speedchange_task);
+	wake_up_process_no_notif(speedchange_task);
 
 rearm_if_notmax:
 	/*
@@ -1626,7 +1626,7 @@ static int __init cpufreq_skateractive_init(void)
 	get_task_struct(speedchange_task);
 
 	/* NB: wake up so the thread does not look hung to the freezer */
-	wake_up_process(speedchange_task);
+	wake_up_process_no_notif(speedchange_task);
 
 	return cpufreq_register_governor(&cpufreq_gov_skateractive);
 }
