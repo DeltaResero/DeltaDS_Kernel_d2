b30a794ac8788cc441cc31496d8d714bb5881291
Shoaib0597 <Shoaib0595@gmail.com>
2017-05-19 01:45:03 -0700

    sched: core: Optimize task_sched_runtime for up to a 20% increase in … …performance
     Signed-off: by Shoaib0597 <Shoaib0595@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1233,6 +1233,20 @@ unsigned long wait_task_inactive(struct task_struct *p, long match_state)
 			cpu_relax();
 		}
 
+#if defined(CONFIG_64BIT) && defined(CONFIG_SMP)
+ /*
+  * 64-bit doesn't need locks to atomically read a 64bit value.
+  * So we have a optimization chance when the task's delta_exec is 0.
+  * Reading ->on_cpu is racy, but this is ok.
+  *
+  * If we race with it leaving cpu, we'll take a lock. So we're correct.
+  * If we race with it entering cpu, unaccounted time is 0. This is
+  * indistinguishable from the read occurring a few cycles earlier.
+  */
+ if (!p->on_cpu)
+ return p->se.sum_exec_runtime;
+#endif
+
 		/*
 		 * Ok, time to look more closely! We need the rq
 		 * lock now, to be *sure*. If we're wrong, we'll
