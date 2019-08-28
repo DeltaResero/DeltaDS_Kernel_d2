66f00cb713dc551461e8a1d14c1b85369fa93fc8
Lonelyoneskatter <threesixoh.skater@yahoo.com>
2019-08-27 21:29:09 -0300

    Increase this to 16 for all I/O schedulers to reduce overhead

--- block/sioplus-iosched.c
+++ block/sioplus-iosched.c
@@ -35,7 +35,7 @@ static const int async_read_expire = HZ * 4;	/* ditto for async, these limits ar
 static const int async_write_expire = HZ * 16;	/* ditto for async, these limits are SOFT! */
 
 static const int writes_starved = 2;		/* max times reads can starve a write */
-static const int fifo_batch     = 1;		/* # of sequential requests treated as one
+static const int fifo_batch     = 16;		/* # of sequential requests treated as one
 						   by the above parameters. For throughput. */
 
 /* Elevator data */
